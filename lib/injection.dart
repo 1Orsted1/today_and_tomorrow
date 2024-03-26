04d60dffee0cecd017a6bd03a47809664f40a2fe
FelipeJTM <felipe.tlaxcala@meddi.mx>
2024-03-25 22:46:40 -0700

    added slang

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:get_it/get_it.dart';
+import 'package:today_and_tomorrow/i18n/strings.g.dart';
 import 'injection.config.dart';
 
 final getIt = GetIt.instance;
@@ -9,4 +10,6 @@ final getIt = GetIt.instance;
   preferRelativeImports: true, // default
   asExtension: true, // default
 )
-void configureDependencies() => getIt.init();
+void configureDependencies() {
+  getIt.init();
+}
